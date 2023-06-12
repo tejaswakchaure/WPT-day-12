845571670816434486d425e76da5cbd79a79ea5e
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-12 12:12:39 +0530

    simple hello world

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return "Hello World";
+//JSX = HTML(predefine tags) in  JS =====> extended HTML===>creating userdefine tag (we can create tag like <App></App>)
+
+function App() {    
+  return    <h1>"Hello World"</h1>;
 }
 
 export default App;
